2cde99d1dc2a1797d51686f1f174393b68ac914b
HorAndr <andyhornackova@gmail.com>
2022-11-27 22:30:31 +0100

    courier walking with box on main hallway

---
+++ Assets/Scripts/playerWalk.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerWalk : MonoBehaviour
+{
+    [SerializeField] private float speed = 2f;
+    private Rigidbody2D body;
+    private Animator anim;
+   
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+       
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        anim.SetBool("walk", horizontalInput != 0);
+        anim.SetBool("left", horizontalInput < -0.01f);
+
+    }
+
+}
