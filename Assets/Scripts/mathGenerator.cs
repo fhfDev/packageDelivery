021db0c091a5c1ddce3d745f83090c41b1fc86b9
Lukáš Maár <luki.m2k@gmail.com>
2023-01-14 13:16:39 +0100

    updated math generator
    updated ref, tagstring, more problems

--- Assets/Scripts/mathGenerator.cs
+++ Assets/Scripts/mathGenerator.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class mathGenerator : MonoBehaviour
 {
-    public Text txt;
-    public InputField result;
-
+    public TextMeshProUGUI textComponent;
+    private InputField result;
+    
     void Start()
     {
         string[] problems = new string[]                    // source: https://cloud-0.edupage.org/cloud/kladne_a_zaporne_cisla.pdf?z%3ALpY%2B0y5zUqIxqqmF56W5kbMiRw6Gar1IjopTiAJhH5aHBEDoqaH2LRaAkmqUpfUv
@@ -18,17 +19,25 @@ public class mathGenerator : MonoBehaviour
             "- 3 + (-5)",
             "(- 2) * 9",
             "10 / (-5)",
-            "10x - 4"
+            "10x - 4",
+            "(+ 4) + (+ 41)",
+            "- 12 + 12",
+            "(- 5) + (- 15)"
         };
         string[] possibleResults = new string[] {"10", "20", "30", "40"};
         System.Random random = new System.Random();
+        textComponent = GetComponent<TextMeshProUGUI>();
+
         int useProblem = random.Next(problems.Length);
-        string selectedProblem = problems[useProblem]; 
-        txt.text = selectedProblem.ToString();              // Debug.Log(selectedProblem);  
-        /*if(result.text.Equals(possibleResults))
-        {
-            Debug.Log("OK");
-        }
-        Debug.Log("Zly vysledok"); */
+        string selectedProblem = problems[useProblem];
+        textComponent.text = selectedProblem.ToString();   // Debug.Log(selectedProblem);
+
+        /*
+            if(result.text.Equals(possibleResults))
+            {
+                Debug.Log("OK");
+            }
+            Debug.Log("Zly vysledok"); 
+        */
     }
 }
